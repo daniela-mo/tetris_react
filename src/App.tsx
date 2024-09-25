4ab81413ee4d5406c2658a4b25ba94a1122d0443
Daniela Oliveira <danni.oliver94@gmail.com>
2024-09-24 21:07:53 -0300

    subindo components

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import Board from './components/Board';
+import {EmptyCell} from './types';
+
+const board = Array(20)
+.fill(null)
+.map
+function App() {
+
+  return (
+<div className="App">
+    </div>
+  )
+}
+
+export default App
